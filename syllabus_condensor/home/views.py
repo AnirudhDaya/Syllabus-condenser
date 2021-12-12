ca5e0621ddbb3d196afec67633a1bb844b73d974
FAHADPN <fahadpuzhakkaraillath@gmail.com>
2021-12-12 13:45:27 +0530

    Syllabus condenser code added

--- syllabus_condensor/home/views.py
+++ syllabus_condensor/home/views.py
@@ -33,7 +33,7 @@ def index(request):
             file1.save()
 
             return HttpResponse("Success..!!")
-
+            
         form1 = UploadQuestions(request.POST,request.FILES)
         form2 = UploadQuestions(request.POST,request.FILES)
         if form1.is_valid() and form2.is_valid():
@@ -48,3 +48,82 @@ def index(request):
             'form2':form2,
         }
     return render(request,'index.html')
+
+
+def predictor():
+    
+    frequency = open('frequency.txt','w+')
+    f = open('ocr.txt','w+')
+
+    pages = convert_from_path('sample.pdf',500)             #convert pdf to pages 
+    image_counter = 1
+    for page in pages:                                      #loop to iterate through pages
+        filename = "page_"+str(image_counter)+".jpg"
+        page.save(filename,'JPEG')                          #saves the pages a jpg files
+        image_counter +=1
+
+    filelimit = image_counter-1                             #set file limit to one less than image counter
+
+
+    custom_config = r'--oem 3 --psm 6'
+    for i in range(1, filelimit + 1):                                       #loop to iterate through each image file of pages 
+        filename = "page_"+str(i)+".jpg"
+        text = str(pytesseract.image_to_string(Image.open(filename),config=custom_config))   #Convert image to text file
+        # text = text.replace('-\n', '')                                      #Replace \n with '' 
+        f.write(text)                                                       #Writes text to file 'f'
+
+    f.seek(0)                                                               #Seeks file pointer to begining
+    wh=['what','pages','who','explain','how','why','when','then','which','where','a','an','or','part','find','answer','and','the','0','1','2','3','4','5','6','7','8','9'] #List of unwanted words
+    punctuations = ['(',')',';',':','[',']',',',"'"]
+    freq = {}                                                                       #frequency dictionary initialized
+    for line in f:                                                                  #Iterates through each line of text file
+        for word in line.split():                                                   #Splits each line with space to aquire words
+            if word not in stopwords.words('english') and word.lower()not in wh and word not in punctuations and word[-1]!=':':    #Checks words to exclude unwanted words
+                word = word.lower()
+                if word in freq:                                                    #Frequency of each word updated to dictionary
+                    freq[word]+=1
+                else:
+                    freq[word]=1
+
+    for w in sorted(freq, key=freq.get, reverse=True):                              #Loop to sort the dictionary
+        w=w+"\n"                                                             
+        frequency.write(w)                                                          #Writes to words to frequency in the sorted order
+
+
+
+    pages_syl = convert_from_path('syllabus.pdf',500)                                   #Converts syllabus to pages
+    image_counter_syl = 1           
+    for page in pages_syl:                                                              #Iterates through each pages
+        filename = "syllabus_"+str(image_counter_syl)+".jpg"                            
+        page.save(filename,'JPEG')                                                  #Saves the file as jpg
+        image_counter_syl +=1
+
+    filelimit= image_counter_syl
+
+    f1 = open('syllabus.txt','w+')                                                 #Opens the file syllabus.txt in appending,reading and writing mode
+    for i in range(1, filelimit):
+        filename = "syllabus_"+str(i)+".jpg"
+        text = str(pytesseract.image_to_string(Image.open(filename),config=custom_config ))        #Convert image to text file
+        # text = text.replace('-\n', '') 
+        f1.write(text)                                                      
+    f1.seek(0)
+    f.seek(0)
+    frequency.seek(0)
+
+    flag=0
+    i=0
+    word_list=[]                                                                        #word_list list initialised 
+    result = open('result.txt','a')                                                     #result text file opened in append mode
+    for word in frequency:                                                              #Iterates through frequency.txt collecting each word
+        for line in f1:                                                                 #Iterates through syllabus.txt collecting each line
+            for w in line.split():                                                      #Splits line by " " and access words  
+                if(word.lower()==w.lower() or word.lower()==w[0:-2].lower()):           #Checks if the word in both files are same
+                    flag=1
+                word_list.append(w)                                                     #Words appended to lsit 
+                if(w[-1]==':'):                                                         #checks
+                    word_list.clear()
+                elif(w[-1] in [',',';']):
+                    s = " "
+                    str = s.join(word_list) + '\n'
+                    result.write(str)
+                    word_list.clear()
